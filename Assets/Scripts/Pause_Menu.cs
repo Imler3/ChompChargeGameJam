a9e8a78445a2a290a945ebca8a2152bb31adbf66
Chowie051 <ashton.d.lakey@gmail.com>
2025-02-22 13:52:03 -0500

    Pause Function
    Added a pause function that pauses the game and audio. It also opens a menu for quit button, etc.

---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Camera;
+    public AudioSource audioSource;
+    public bool Paused;
+
+
+    
+
+    void start()
+    {
+        Canvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown ("escape"))
+        {
+            if (Paused == true)
+            {
+                Time.timeScale = 1.0f;
+                Canvas.gameObject.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                audioSource.Play(0);
+                Paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                audioSource.Pause();
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        Canvas.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        GetComponent<AudioSource>();
+    }
+}
